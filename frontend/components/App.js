88fbb482df81b8e8af9be4a14c2a526b0cce47fe
mkaufman385 <mkaufman385@gmail.com>
2023-09-27 10:11:42 -0400

    new update 9/27 @10:11am

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -19,16 +19,18 @@ export default class App extends React.Component {
     this.setState({ ...this.state, todoNameInput: "" });
   };
 
+  setAxiosResponseError = (err) => {
+    this.setState({ ...this.state, error: err.response.data.message });
+  };
+
   postNewTodo = () => {
     axios
       .post(URL, { name: this.state.todoNameInput })
       .then((res) => {
         this.fetchAllTodos();
-        this.setState({ ...this.state, todoNameInput: "" });
+        this.resetForm();
       })
-      .catch((err) => {
-        this.setState({ ...this.state, error: err.response.data.message });
-      });
+      .catch(this.setAxiosResponseError);
   };
 
   onTodoFormSubmit = (evt) => {
@@ -42,9 +44,7 @@ export default class App extends React.Component {
       .then((res) => {
         this.setState({ ...this.state, todos: res.data.data });
       })
-      .catch((err) => {
-        this.setState({ ...this.state, error: err.response.data.message });
-      });
+      .catch(this.setAxiosResponseError);
   };
   componentDidMount() {
     this.fetchAllTodos();
