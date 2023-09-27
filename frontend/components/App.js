5e268f860e5600adf270ba269011c7d5fdd16467
mkaufman385 <mkaufman385@gmail.com>
2023-09-27 06:50:55 -0400

    updated 9/27

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,6 +6,13 @@ const URL = "http://localhost:9000/api/todos";
 export default class App extends React.Component {
   state = {
     todos: [],
+    error: "",
+    todoNameInput: "",
+  };
+
+  onTodoNameInputChange = (evt) => {
+    const { value } = evt.target;
+    this.setState({ ...this.state, todoNameInput: value });
   };
 
   fetchAllTodos = () => {
@@ -15,7 +22,7 @@ export default class App extends React.Component {
         this.setState({ ...this.state, todos: res.data.data });
       })
       .catch((err) => {
-        debugger;
+        this.setState({ ...this.state, error: err.response.data.message });
       });
   };
   componentDidMount() {
@@ -24,7 +31,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <div id="error">Error: No error here</div>
+        <div id="error">Error: {this.state.error}</div>
         <div id="todos">
           <h2> Todos:</h2>
           {this.state.todos.map((td) => {
@@ -32,7 +39,12 @@ export default class App extends React.Component {
           })}
         </div>
         <form id="todoForm">
-          <input type="text" placeholder="Type todo"></input>
+          <input
+            value={this.state.todoNameInput}
+            onChange={this.onTodoNameInputChange}
+            type="text"
+            placeholder="Type todo"
+          ></input>
           <input type="submit"></input>
           <button>Clear Completed</button>
         </form>
